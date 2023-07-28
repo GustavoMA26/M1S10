99e394c3c73f222263eaa16eff1e509c4649851c
Gustavo <gmalmeida26@gmail.com>
2023-07-28 15:22:12 -0300

    Ex 7 - Criação de componente CardMedicamento

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from "./Components/Header"
 import FormularioNovoMedicamento from './Components/FormularioNovoMedicamento'
 import { MedicamentosContextProvider } from './Components/Context/MedicamentosContext'
+import CardMedicamento from './Components/CardMedicamento'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Header />
     <MedicamentosContextProvider>
       <FormularioNovoMedicamento />
+      <CardMedicamento medicamento ={{nome: 'Dorflex', laboratorio: 'Clamed', preco: 9.99, favorito: false, id: 1}} />
     </MedicamentosContextProvider>
     </>
   )
---
+++ src/Components/CardMedicamento/index.jsx
@@ -0,0 +1,14 @@
+//function CardMedicamento ({nome, laboratorio, preco, favorito}){ //Passando por props
+import './index.css'
+function CardMedicamento ({medicamento}) { // Passando por objeto, através do qual se acessará os dados
+    return (
+        <div className="card">
+            <p>{medicamento.favorito}</p>
+            <img width = {200} src="https://img.freepik.com/vetores-gratis/frasco-de-medicamento-em-branco_1284-4087.jpg?w=826&t=st=1690567417~exp=1690568017~hmac=5db4cbfb36ea1839c815f5cae02bbe8afe3755db459df48a67cc26cf137c9dcd" alt="Frasco de Medicamento"></img>
+            <h2>Medicamento: {medicamento.nome}</h2>
+            <h3>Laboratório: {medicamento.laboratorio}</h3>
+            <h4>R${medicamento.preco}</h4>
+        </div>
+    )
+}
+export default CardMedicamento
