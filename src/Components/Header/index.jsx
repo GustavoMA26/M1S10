1bfb63b6efbfad7fea225bb77c73add598734927
Gustavo <gmalmeida26@gmail.com>
2023-07-27 21:46:01 -0300

    Ex 2 - Criação componente Header

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+ import  "./index.css"
+ 
+ function Header () {
+    return (
+        <nav className="header"> 
+            <img className="logo" src="src/assets/logo_FarmaDEV.svg" width="75" height="60" alt='Logo FarmaDev'></img>
+        </nav>
+
+    )
+}
+
+export default Header
